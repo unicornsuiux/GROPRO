0680e0cc8bce868d2400728c19c252258f015d4a
danish334 <danish@cynosuredesigns.co.uk>
2023-03-24 01:38:47 +0500

    updated

--- frontend/js/main-custom.js
+++ frontend/js/main-custom.js
@@ -42,6 +42,25 @@ $(document).ready(function() {
 });
 
 
+// CHANGE PRICING
+function check() {
+  var checkBox = document.getElementById("checbox");
+  var text1 = document.getElementsByClassName("text1");
+  var text2 = document.getElementsByClassName("text2");
+
+  for (var i = 0; i < text1.length; i++) {
+    if (checkBox.checked == true) {
+      text1[i].style.display = "block";
+      text2[i].style.display = "none";
+    } else if (checkBox.checked == false) {
+      text1[i].style.display = "none";
+      text2[i].style.display = "block";
+    }
+  }
+}
+check();
+
+
 
 // ANIMATION
 wow = new WOW(
